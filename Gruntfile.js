7645929f12242d9bd2f40acab134bf10fab0363b
Kriss Watt <kriss.watt@gmail.com>
2013-11-13 08:47:56 +0000

    Project created

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          sourcemap: false
+        },
+        files: {
+          'stylesheets/css/main.css': 'stylesheets/sass/main.scss'
+        }
+      }
+    },
+
+    watch: {
+      files: ['stylesheets/sass/**/*.scss'],
+      tasks: ['sass']
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: '.'
+        }
+      }
+    }
+  });
+
+  // Modules
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Tasks
+  grunt.registerTask('default', ['connect', 'watch']);
+};
